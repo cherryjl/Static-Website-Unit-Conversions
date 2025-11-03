9ec48e686af3d472671c91cd8ed59566a4ad39e1
cherryjl <jlcherry1132@gmail.com>
2025-11-03 12:14:11 -0500

    Added a dark mode switch
    Need to still adjust dark mode colors

--- js/appearanceToggle.js
+++ js/appearanceToggle.js
@@ -1,11 +1,29 @@
 if (localStorage.getItem('theme') === 'dark') {
     document.documentElement.classList.add('dark-mode');
+    document.getElementById('modeIcon').src = 'images/moon_png';
 }
 
-const toggleBtn = document.getElementById('theme-toggle');
+const toggleBtn = document.getElementById('modeIcon');
 const html = document.documentElement;
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    html.classList.add('dark-mode');
+    toggleBtn.src = 'images/moon_icon.png';
+} else {
+    html.classList.remove('dark-mode');
+    toggleBtn.src = 'images/sun_icon.png';
+}
 
 toggleBtn.addEventListener('click', () => {
     html.classList.toggle('dark-mode');
-    localStorage.setItem('theme', html.classList.contains('dark-mode') ? 'dark' : 'light');
+
+    if (html.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+        toggleBtn.src = 'images/moon_icon.png';
+    } else {
+        localStorage.setItem('theme', 'light');
+        toggleBtn.src = 'images/sun_icon.png';
+    }
 });
